8a0f6a96b8d6e8f1e8719fed316ce71bdb62c4a0
ZonRed <myansar70@gmail.com>
2023-11-13 02:40:59 +0700

    main

--- resources/views/pengguna/index.blade.php
+++ resources/views/pengguna/index.blade.php
@@ -143,13 +143,13 @@
         <p>&copy; 2023 Portal Berita Mang Ujang</p>
     </footer>
 
-    <!-- Tambahkan tautan ke file JavaScript Bootstrap -->
+    {{-- <!-- Tambahkan tautan ke file JavaScript Bootstrap -->
     <!-- <link rel="stylesheet" href="{{asset('js/bootstrap.js')}}">
     <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}">
     <link rel="stylesheet" href="{{asset('js/bootstrap.bundle.js')}}">
     <link rel="stylesheet" href="{{asset('js/bootstrap.bundle.min.js')}}">
     <link rel="stylesheet" href="{{asset('js/bootstrap.esm.js')}}">
-    <link rel="stylesheet" href="{{asset('js/bootstrap.esm.min.js')}}"> -->
+    <link rel="stylesheet" href="{{asset('js/bootstrap.esm.min.js')}}"> --> --}}
 
    <!-- Tambahkan tautan ke file JavaScript Bootstrap -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
