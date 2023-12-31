dd591ab2dfe6dfb3aa193ae133045dc001f0176d
ZonRed <myansar70@gmail.com>
2023-12-31 17:22:53 +0700

    bingung

--- resources/views/pengguna/statistik.blade.php
+++ resources/views/pengguna/statistik.blade.php
@@ -194,8 +194,8 @@
     </script>
 
     <!-- Tambahkan tautan ke file JavaScript Bootstrap -->
-    <!-- <link rel="stylesheet" href="{{asset('js/bootstrap.js')}}">
-    <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}"> -->
+    {{-- <link rel="stylesheet" href="{{asset('js/bootstrap.js')}}">
+    <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}"> --}}
     
     <!-- Tambahkan tautan ke file JavaScript Bootstrap -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
